97d65b3232e7b27e5345981548ada57b5ff9df8a
André David dos Santos <aetdesenvolvimentoweb@gmail.com>
2025-11-20 10:49:18 -0300

    refactor: improving files organization

---
+++ src/shared/usecases/crud/index.ts
@@ -0,0 +1,5 @@
+export * from "./base.create.usecase";
+export * from "./base.delete.usecase";
+export * from "./base.find-by-id.usecase";
+export * from "./base.get-all.usecase";
+export * from "./base.update.usecase";
--- src/shared/usecases/index.ts
+++ src/shared/usecases/index.ts
@@ -1,5 +1,1 @@
-export * from "./base.create.usecase";
-export * from "./base.delete.usecase";
-export * from "./base.find-by-id.usecase";
-export * from "./base.get-all.usecase";
-export * from "./base.update.usecase";
+export * from "./crud";
